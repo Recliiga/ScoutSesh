473ccfde263acbc0b776169555e05dadc0ac53b1
Great <greatochuko123@gmail.com>
2024-11-07 12:03:54 +0100

    add log messages to test for getSession bug on production environment

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -4,11 +4,13 @@ export async function getSession() {
   try {
     const cookieStore = await cookies();
     const token = cookieStore.get("token")?.value;
+    console.log("Token:", token);
     const res = await fetch("http://localhost:3000/api/auth/session", {
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await res.json();
-    console.log(data);
+    console.log("Data from get Session function", data);
+
     if (data.error) {
       return { user: null, error: data.error };
     } else {
