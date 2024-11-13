d1833629d3ccc324d4c117a630b385c678b5e557
Great <greatochuko123@gmail.com>
2024-11-13 14:11:31 +0100

    rename Athlete Team Members page file

--- src/app/app/team-members/page.tsx
+++ src/app/app/team-members/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { getSession } from "@/services/authServices";
 import CoachTeamMembersPage from "@/components/pages/CoachTeamMembersPage";
-import AthleteTeamMembersPage from "@/components/pages/AthleteTeamMembersPage copy";
+import AthleteTeamMembersPage from "@/components/pages/AthleteTeamMembersPage";
 
 export default async function TeamMembersPage() {
   const { user } = await getSession();
