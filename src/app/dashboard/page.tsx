3291fc639ccc20b9fa3458f56564fe203b4a7875
Great <greatochuko123@gmail.com>
2024-11-18 01:42:42 +0100

    get BASE_URL from headers not .env

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import CoachDashboard from "@/components/pages/CoachDashboard";
 import { getSession } from "@/services/authServices";
 import AthleteDashboard from "@/components/pages/AthleteDashboard";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
   const { user } = await getSession();
 
-  if (!user) return;
+  if (!user) redirect("/login");
 
   if (user.role === "Athlete") {
     return <AthleteDashboard user={user} />;
