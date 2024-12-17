6f43a2fa2b779b6ed4da7c6607a158daf661fef4
Great <greatochuko123@gmail.com>
2024-12-17 02:05:07 +0100

    redesign feature section

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -10,17 +10,17 @@ export default function FeatureSection({
   imageSrc: string;
 }) {
   return (
-    <div className="flex xl:flex-row flex-col items-center gap-6 bg-white shadow-md p-3 sm:p-6 rounded-lg">
-      <div className="relative flex-1 w-full aspect-video xl:aspect-auto h-full min-h-40">
+    <div className="flex flex-col items-center gap-6 rounded-lg bg-white p-3 shadow-md sm:p-6 xl:flex-row">
+      <div className="relative aspect-video h-full min-h-40 w-full flex-1 xl:aspect-auto">
         <Image
           src={imageSrc}
           alt={title}
-          className="rounded-lg w-full h-full object-cover"
+          className="h-full w-full rounded-lg object-cover"
           fill
         />
       </div>
-      <div className="flex-1">
-        <h2 className="mb-2 font-semibold text-2xl text-green-600">{title}</h2>
+      <div className="xl:flex-1">
+        <h2 className="mb-2 text-2xl font-semibold text-green-600">{title}</h2>
         <p className="text-gray-600">{description}</p>
       </div>
     </div>
