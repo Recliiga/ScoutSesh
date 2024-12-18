4075ce475aeb73902b971ba5db4ed2e98e43b362
Great <greatochuko123@gmail.com>
2024-12-18 17:20:21 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { getSession } from "@/services/authServices";
 import Footer from "@/components/Footer";
 import { fetchLatestInvitationCode } from "@/services/invitationServices";
 
+//comment
+
 export const metadata: Metadata = {
   title: "ScoutSesh",
   description:
