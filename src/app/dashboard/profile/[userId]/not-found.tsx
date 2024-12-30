3c3a0ebb5c1b34634865ab35d6c425b8573fb80b
Great Ogheneochuko <greatochuko123@gmail.com>
2024-12-30 21:59:37 +0100

    create custom 404 page for user profile route

---
+++ src/app/dashboard/profile/[userId]/not-found.tsx
@@ -0,0 +1,35 @@
+import { getSessionFromHeaders } from "@/services/authServices";
+import Link from "next/link";
+import React from "react";
+
+export default async function NotFoundPage() {
+  const user = await getSessionFromHeaders();
+
+  return (
+    <main className="flex-center flex-1 text-accent-black">
+      <div className="flex-center mx-auto w-[90%] max-w-sm flex-col gap-2 text-center sm:gap-3">
+        <h2 className="text-2xl font-bold text-green-600 sm:text-3xl">OOPS!</h2>
+        <h1 className="font-semibold">USER NOT FOUND</h1>
+        <p className="text-sm text-accent-gray-300 sm:text-base">
+          The user you are looking for might have been removed, or is
+          temporarily unavailable.
+        </p>
+        {user ? (
+          <Link
+            href={"/dashboard"}
+            className="rounded-full bg-green-600 px-4 py-2 text-sm font-bold text-white duration-300 hover:bg-green-700"
+          >
+            GO TO DASHBOARD
+          </Link>
+        ) : (
+          <Link
+            href={"/"}
+            className="rounded-full bg-green-600 px-4 py-2 text-sm font-bold text-white duration-300 hover:bg-green-700"
+          >
+            GO HOME
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+}
