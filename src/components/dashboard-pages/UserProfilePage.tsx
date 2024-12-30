8bd039bf8f813fa096a38dc9d975e21ac24ff395
Great Ogheneochuko <greatochuko123@gmail.com>
2024-12-30 22:07:08 +0100

    fix edit profile input width

--- src/components/dashboard-pages/UserProfilePage.tsx
+++ src/components/dashboard-pages/UserProfilePage.tsx
@@ -158,7 +158,7 @@ export default function UserProfilePage({
           </div>
           <CardHeader className="px-4 pb-0 pt-20 sm:px-6">
             <div className="flex flex-wrap items-start justify-between gap-4">
-              <div className="">
+              <div className="flex-1">
                 {isEditing ? (
                   <div className="flex w-full gap-2">
                     <input
